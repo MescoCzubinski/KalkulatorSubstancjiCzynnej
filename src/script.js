630b1c08038c4d58bcd9569383ec28108748ff8b
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-12-04 12:08:15 +0100

    to test version

--- src/script.js
+++ src/script.js
@@ -10,18 +10,14 @@ visited.forEach((input) => {
 });
 function textToNumber(variable) {
   variable = variable.replace(/[^0-9.,]/g, "");
-  if (variable.includes(",")) {
-    variable = variable.replace(",", ".");
-  }
+  if (variable.includes(",")) variable = variable.replace(",", ".");
   return oneDot(variable);
 }
 
 function oneDot(variable) {
   const firstDotIndex = variable.indexOf(".");
 
-  if (firstDotIndex === -1) {
-    return variable;
-  }
+  if (firstDotIndex === -1) return variable;
   if (firstDotIndex === 0) {
     variable = "0.";
     return variable;
@@ -29,7 +25,44 @@ function oneDot(variable) {
   const beforeDot = variable.slice(0, firstDotIndex + 1);
   let afterDot = variable.slice(firstDotIndex + 1);
   afterDot = afterDot.replace(".", "");
-  afterDot = afterDot.slice(0, 2);
+  afterDot = afterDot.slice(0, 3);
 
   return beforeDot + afterDot;
 }
+function calculation(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  return (a * b) / c;
+}
+let elementIngredientContentsOther = document.querySelector("#ingredient-contents-other");
+let elementIngredientContentsOtherUnit = document.querySelector("#ingredient-contents-other-unit");
+let elementDoseContents = document.querySelector("#dose-contents");
+let elementDoseContentsUnit = document.querySelector("#dose-contents-unit");
+let elementIngredientContentsPolish = document.querySelector("#ingredient-contents-polish");
+let elementIngredientContentsPolishUnit = document.querySelector("#ingredient-contents-polish-unit");
+let elementResult = document.querySelector("#result");
+
+document.addEventListener("input", function () {
+  let result = 0;
+
+  let otherIngredients = textToNumber(elementIngredientContentsOther.value);
+  elementIngredientContentsOther.value = otherIngredients;
+
+  let doseContents = textToNumber(elementDoseContents.value);
+  elementDoseContents.value = doseContents;
+
+  let polishIngredients = textToNumber(elementIngredientContentsPolish.value);
+  elementIngredientContentsPolish.value = polishIngredients;
+
+  result = calculation(otherIngredients, doseContents, polishIngredients);
+  if (elementIngredientContentsPolishUnit.value === "%") {
+    result /= 10;
+  }
+  if (elementIngredientContentsOtherUnit.value === "%") {
+    result *= 10;
+  }
+
+  result = Math.round(result * 1000) / 1000;
+  if (result !== Infinity && result !== 0 && !isNaN(result)) elementResult.innerHTML = result + " " + elementDoseContentsUnit.value;
+});
